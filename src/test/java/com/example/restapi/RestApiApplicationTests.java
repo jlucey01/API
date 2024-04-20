1cffc80fd5e595cddc30aff262edd52a72a3ea7f
jlucey01 <jlucey@icloud.com>
2024-04-20 10:30:34 -0400

    Remove Test file from structure

--- src/test/java/com/example/restapi/RestApiApplicationTests.java
+++ src/test/java/com/example/restapi/RestApiApplicationTests.java
@@ -2,12 +2,22 @@ package com.example.restapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RestApiApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
 
     @Test
     void contextLoads() {
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/hooray", String.class))
+                .isEqualTo("Hooray, it works! ");
     }
-
 }
